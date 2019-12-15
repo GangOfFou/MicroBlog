ff0b3aed6f85800a8723804825d26b9c5543b603
Dariusz Skirzynski <myjobds@gmail.com>
2019-12-15 12:26:15 +0100

    Adedd classes Post, User

---
+++ src/main/java/com/sda/MicroBlog/Model/Post.java
@@ -0,0 +1,52 @@
+package com.sda.MicroBlog.Model;
+
+import java.util.Date;
+
+public class Post {
+
+    private Long id;
+    private String title;
+    private String body;
+    private User author;
+    private Date date = new Date();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
